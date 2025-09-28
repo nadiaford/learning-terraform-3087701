4d06178d447a76b8442363960144ae55e79895ba
Nadia Ford <68981493+nadiaford@users.noreply.github.com>
2025-09-28 15:23:17 -0400

    Updated local zones

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ module "web_vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2", "us-west-2", "us-west-2"]
+  azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
 
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
